91915944110e6d627c0709ca8e3103acef02dc51
lbBFHS <lbriand27@bfhsla.org>
2026-03-10 15:30:53 -0500

    s

--- scenes/processes/var_jump_component.gd
+++ scenes/processes/var_jump_component.gd
@@ -3,24 +3,49 @@ extends Node
 
 @export_subgroup("Nodes")
 @export var jump_buffer_timer: Timer
+@export var coyote_time_timer: Timer
 
 @export_subgroup("Settings")
 @export var jump_velocity: float = -350.0
 
 var is_going_up: bool = false
+var is_jumping: bool = false
+var last_frame_on_floor: bool = false
+
+func has_just_landed(body: CharacterBody2D) -> bool:
+	return body.is_on_floor() and not last_frame_on_floor and is_jumping
+
+func is_allowed_to_jump(body: CharacterBody2D, want_to_jump: bool) -> bool:
+	return want_to_jump and (body.is_on_floor() or not coyote_time_timer.is_stopped())
 
 func handle_jump(body: CharacterBody2D, want_to_jump: bool, jump_released: bool) -> void:
-	if want_to_jump and body.is_on_floor():
-		jump(body)	
-		
+	if has_just_landed(body):
+		is_jumping = false
+	
+	if is_allowed_to_jump(body, want_to_jump):
+		jump(body)
+	
+	handle_coyote_time(body)
 	handle_jump_buffer(body, want_to_jump)
 	handle_variable_jump_height(body, jump_released)
 	
 	is_going_up = body.velocity.y < 0 and not body.is_on_floor()
-	
+	last_frame_on_floor = body.is_on_floor()
+func has_just_stepped_off_ledge(body: CharacterBody2D) -> bool:
+	return not body.is_on_floor() and last_frame_on_floor and not is_jumping
+
+func handle_coyote_time(body: CharacterBody2D) -> void:
+	if has_just_stepped_off_ledge(body):
+		coyote_time_timer.start()
+		
+	if not coyote_time_timer.is_stopped() and not is_jumping:
+		body.velocity.y = 0
+
 func jump(body: CharacterBody2D) -> void:
 	body.velocity.y = jump_velocity
 	jump_buffer_timer.stop()
+	is_jumping = true
+	coyote_time_timer.stop()
 
 func handle_variable_jump_height(body: CharacterBody2D, jump_released: bool) -> void:
 	if jump_released and is_going_up:
