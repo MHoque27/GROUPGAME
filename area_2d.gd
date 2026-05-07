1c774f8d3da16599309fc0a8dcd0af6fed6f5798
MHoque27 <159090598+MHoque27@users.noreply.github.com>
2026-05-07 15:28:52 -0500

    added collectable clock
    aahhhhh

---
+++ area_2d.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var target_scene: String
+@export var prompt_text: String = "Enter"
+
+func interact(player):
+	print("INTERACT CALLED")
+	print("SCENE:", target_scene)
+
+	if Global.game:
+		print("CALLING GAME MANAGER")
+		$AudioStreamPlayer.play()
+		#$Timer.start()
+		#await $Timer.timeout
+		Global.game.change_scene(target_scene)
+	else:
+		print("GLOBAL GAME IS NULL")
+		
---
+++ clock.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+
+@export var prompt_text: String = "Enter"
+#@onready var uitool = get_tree().get_first_node_in_group("ui_tool")
+func interact(player):
+	print("INTERACT CALLED")
+	print("SCENE:",)
+	var clock = get_tree().current_scene.find_child("ClockUi", true)
+	var transition_controller = get_tree().current_scene.find_child("TransitionController", true)
+	var transition_in = "fade_in"
+	var transition_out = "fade_out"
+	var seconds = 1.5
+	transition_controller.transition(transition_in, seconds)
+	await transition_controller.animation_player.animation_finished
+	
+	$AnimationPlayer.play("obtain_In")
+	$AudioStreamPlayer.play()
+	await $AnimationPlayer.animation_finished
+	
+	$AnimationPlayer.play("obtain_In2")
+	await $AnimationPlayer.animation_finished
+	
+	$Timer.start()
+	await $Timer.timeout
+	
+	$AnimationPlayer.play("obtain_OutBoth")
+	await $AnimationPlayer.animation_finished
+	$AudioStreamPlayer.stop()
+	transition_controller.transition(transition_out, seconds)
+	clock.visible = true
+	queue_free()
+	await transition_controller.animation_player.animation_finished
+
+	
