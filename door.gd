626c5d55e1fe5af03568c2c5336c8ea46ec0b615
MHoque27 <159090598+MHoque27@users.noreply.github.com>
2026-05-12 15:30:00 -0500

    notes
    aaaa

--- door.gd
+++ door.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var target_scene: String
 @export var prompt_text: String = "Enter"
-
+@export var player_spawn_position = Vector2()
 func interact(player):
 	print("INTERACT CALLED")
 	print("SCENE:", target_scene)
@@ -13,6 +13,7 @@ func interact(player):
 		#$Timer.start()
 		#await $Timer.timeout
 		Global.game.change_scene(target_scene)
+		#then change player position from global
 	else:
 		print("GLOBAL GAME IS NULL")
 		
--- global.gd
+++ global.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var game : GameController
+var player_spawn_pos #vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
